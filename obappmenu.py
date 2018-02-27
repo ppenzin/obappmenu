d41d0597e92b929a069c97e5d428681bd14734b7
Petr Penzin <penzin.dev@gmail.com>
2018-02-27 07:33:06 -0800

    Better menu representation

--- obappmenu.py
+++ obappmenu.py
@@ -2,14 +2,12 @@
 
 The goal is to get a simple (two-level menu) that would agreggate available applications by user-defined categorie.
 
-The format for an item is as follows:
-    'name': display name (optional)
-    'command': command to execute (will be displayed if no name is given)
-    'category': first-level menu to add this item to
+Items are stored as a dictionary with 'leaf' entries treated as executable
+commands.
 
 TODO:
     - Aggregate menu from two locations (system-wide and user)
-    - Switch input to Yaml or use a better format
+    - Consider switching input to Yaml or JSON
 """
 
 try:
@@ -17,39 +15,34 @@ try:
 except ImportError:
   from xml.etree import ElementTree as etree
 
+def walk(node, parent):
+  """ Recursively produce XML for nested dictionary"""
+  for name, item in node.iteritems():
+    if type(item) is dict:
+      cat = etree.SubElement(parent, 'menu', id=name, label=name)
+      walk (item, cat)
+    else:
+      menu_item = etree.SubElement(parent, 'item', label=name)
+      action = etree.SubElement(menu_item, 'action', name='execute')
+      etree.SubElement(action, 'command').text = item
+
 class OpenBoxAppMenu:
   def __init__(self):
-    self.menuItems = []
+    self.menuItems = {}
   
   def add(self, data):
     """ Add contents of items array to the internal list"""
-    self.menuItems += data
+    self.menuItems.update(data)
   
   def flush(self):
     """ Clear accumulated data """
-    self.menuItems = []
+    self.menuItems = {}
 
   def render(self):
     """ Produce XML for openbox pipe menu based on the items"""
-    # Root menu element
     menu = etree.Element('openbox_pipe_menu')
-    # App categories
-    categories = {}
     
-    # Compose the menu
-    for item in self.menuItems:
-      cat = item['category']
-      if (not cat in categories):
-        menu_category = etree.SubElement(menu, 'menu', id=cat, label=cat)
-        categories[cat] = menu_category
-      # Name is optional
-      if ('name' in item):
-        menu_label = item['name']
-      else:
-        menu_label = item['command']
-      menu_item = etree.SubElement(categories[cat], 'item', label=menu_label)
-      action = etree.SubElement(menu_item, 'action', name='execute')
-      etree.SubElement(action, 'command').text = item['command']
+    walk(self.menuItems, menu)
       
     print etree.tostring(menu)
 
